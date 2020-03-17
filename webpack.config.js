a35027935209ff0cd9ccd6fe31b24735fe76adfd
Mahathir Jeshan <m.m.jeshan@gmail.com>
2020-03-18 03:52:09 +0600

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// Webpack uses this to work with directories
+const path = require('path');
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './src/javascript/index.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'theme/assets'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /theme/],
+        use: ['babel-loader']
+      }
+    ]
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript
+  // minifying and other thing so let's set mode to development
+  mode: 'production',
+  watch: true
+};
